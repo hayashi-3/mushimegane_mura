48bf9b93589ec581d596d330d30f16198454b69c
vagrant <vagrant@localhost.localdomain>
2020-10-11 15:57:53 +0000

    管理側予約状況画面追加

--- app/controllers/organizers/reservations_controller.rb
+++ app/controllers/organizers/reservations_controller.rb
@@ -2,17 +2,14 @@ class Organizers::ReservationsController < ApplicationController
   before_action :authenticate_organizer!
 
   def index
-  end
-
-  def show
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:event_id])
     @reservations = @event.reservations.all
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:participant_id, :event_id, :attendance_status)
+    params.permit(:participant_id, :event_id, :attendance_status)
   end
 
 end
