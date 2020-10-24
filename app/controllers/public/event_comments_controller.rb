0e49743af8ba8515ecbfaf3bc519a4eed9ed1d2b
vagrant <vagrant@localhost.localdomain>
2020-10-24 08:22:16 +0000

    デプロイ修正

--- app/controllers/public/event_comments_controller.rb
+++ app/controllers/public/event_comments_controller.rb
@@ -2,14 +2,14 @@ class Public::EventCommentsController < ApplicationController
     before_action :authenticate_participant!
 
 	def create
-	  @event = Event.find(params[:event_id])
+	  @event = Event.where(id: params[:id])
 	  @event_comment = current_participant.event_comments.new(event_comment_params)
 	  @event_comment.participant_id = current_participant.id
 	  @event_comment.event_id = @event.id
 	  if @event_comment.save
 	    redirect_back(fallback_location: root_path)
 	  else
-		redirect_back(fallback_location: root_path)
+		redirect_back(fallback_location: event_path(@event))
 	  end
 	end
 
